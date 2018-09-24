2b4c1c61d74030f1511ac2786855cc882318ebdf
d.antonov <d.antonov@corp.mail.ru>
2018-09-24 20:50:51 +0300

    AWM-17688 Added clear and forward methods for local notifications with IOS implementation, badge and sound config

--- Source/PsPushNotificationsExtended/Public/PsPushNotificationsExtendedManager.h
+++ Source/PsPushNotificationsExtended/Public/PsPushNotificationsExtendedManager.h
@@ -17,27 +17,50 @@ class UPsPushNotificationsExtendedManager : public UObject
 	GENERATED_UCLASS_BODY()
 
 public:
-	// Called when user activates some action from push notifications
+	/// Called when user activates some action from push notifications
 	UPROPERTY(BlueprintAssignable)
 	FPsOnActionPerformDelegate OnActionPerform;
 
+	/// Get instance of the manager
 	UFUNCTION(BlueprintCallable)
 	static UPsPushNotificationsExtendedManager* GetInstance();
 
+	/// Creates an instance of the class
 	UFUNCTION(BlueprintCallable)
 	static void CreateInstance();
 
+	/// Destructor method
 	~UPsPushNotificationsExtendedManager() throw() override;
 
+	/// Requests authority for push notifications
 	UFUNCTION(BlueprintCallable)
 	void RequestPushNotifications();
 
+	/// Adds notifications cathegory
 	UFUNCTION(BlueprintCallable)
 	void AddNotificationCategory(const FString& Name, const TArray<FPsNotificationsAction>& Actions);
 
+	/** Sends local notifications with a specified content in a period of time in seconds
+	 @return Notification ID
+	 */
 	UFUNCTION(BlueprintCallable)
-	void SendLocalNotificationFromNow(float SecondsFromNow, const FPsNotification& Notification);
+	FString SendLocalNotificationFromNow(float SecondsFromNow, const FPsNotification& Notification);
 
+	/** Sends local notifications with a specified conted at specified time
+	 @return Notification ID
+	 */
 	UFUNCTION(BlueprintCallable)
-	void SendLocalNotification(const FDateTime& DateTime, bool bLocalTime, const FPsNotification& Notification);
+	FString SendLocalNotification(const FDateTime& DateTime, bool bLocalTime, const FPsNotification& Notification);
+
+	/// Cleares all notifications
+	UFUNCTION(BlueprintCallable)
+	void ClearAllLocalNotifications();
+
+	/// Removes local notification with specified ID
+	UFUNCTION(BlueprintCallable)
+	void ClearLocalNotificationsWithId(const TArray<FString>& NotificationsIds);
+
+	/// Gets last notification action id (use at start of the application to navigate user action)
+	UFUNCTION(BlueprintCallable)
+	FString GetLastNotificationActionId();
 };
