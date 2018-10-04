6669ee8c7e0ecf2361737749be4210c5252423a7
d.antonov <d.antonov@corp.mail.ru>
2018-10-04 20:10:35 +0300

    AWM-17687 android push notifications media content && forward action meta data to application

--- Source/PsPushNotificationsExtended/Java/PsLocalNotificationsExtendedReceiver.java
+++ Source/PsPushNotificationsExtended/Java/PsLocalNotificationsExtendedReceiver.java
@@ -10,6 +10,12 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.HttpURLConnection;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 
 public class PsLocalNotificationsExtendedReceiver extends BroadcastReceiver
 {
@@ -20,6 +26,9 @@ public class PsLocalNotificationsExtendedReceiver extends BroadcastReceiver
 		String details  = intent.getStringExtra("local-notification-body");
 		String action = intent.getStringExtra("local-notification-action");
 		String activationEvent = intent.getStringExtra("local-notification-activationEvent");
+		String contentURL = intent.getStringExtra("local-notification-contentURL");
+
+		Log.d("onReceive", "NOID: " + notificationID);
 
 		if(title == null || details == null || action == null || activationEvent == null)
 		{
@@ -27,32 +36,8 @@ public class PsLocalNotificationsExtendedReceiver extends BroadcastReceiver
 			return;
 		}
 
-		// Open UE4 app if clicked
-		Intent notificationIntent = new Intent(context, GameActivity.class);
-
-		// launch if closed but resume if running
-		notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-		notificationIntent.putExtra("localNotificationID" , notificationID);
-		notificationIntent.putExtra("localNotificationAppLaunched" , true);
-		notificationIntent.putExtra("localNotificationLaunchActivationEvent", activationEvent);
-
-		int notificationIconID = context.getResources().getIdentifier("ic_notification", "drawable", context.getPackageName());
-		if (notificationIconID == 0)
-		{
-			notificationIconID = context.getResources().getIdentifier("icon", "drawable", context.getPackageName());
-		}
-		PendingIntent pendingNotificationIntent = PendingIntent.getActivity(context, notificationID, notificationIntent, 0);
-
-		NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
-			.setSmallIcon(notificationIconID)
-			.setContentIntent(pendingNotificationIntent)
-			.setWhen(System.currentTimeMillis())
-			.setTicker(details)		// note: will not show up on Lollipop up except for accessibility
-			.setContentTitle(title)
-			.setContentText(details)
-			.setVisibility(NotificationCompat.VISIBILITY_PUBLIC);
-
 		// Checking action buttons
+		String buttonsContext = "";
 		for (int i = 0;;i++)
 		{
 			String actionTitleExtraTag = "local-notification-cateroty-action-" + i + "-title";
@@ -64,37 +49,152 @@ public class PsLocalNotificationsExtendedReceiver extends BroadcastReceiver
 				break;
 			}
 
-			Intent notificationActionIntent = new Intent(context, PsLocalNotificationsExtendedActionReceiver.class);
-//			notificationActionIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-			notificationActionIntent.putExtra("local-notification-ID", notificationID);
-			notificationActionIntent.putExtra("local-notification-action", action);
-			notificationActionIntent.putExtra("local-notification-activationEvent", activationEvent);
-			notificationActionIntent.putExtra("local-notification-activationAction", actionId);
-			notificationActionIntent.setAction("com.my.PsLocalNotificationsExtendedActionReceiver.Broadcast");
+			buttonsContext += actionTitle + "=" + actionId + "|";
+		}
+
+		new sendNotification(context).execute(String.valueOf(notificationID), title, details, action, activationEvent, contentURL, buttonsContext);
+	}
+
+	private class sendNotification extends AsyncTask<String, Void, Bitmap>
+	{
+		Context context;
+		int notificationID;
+		String title;
+		String details;
+		String action;
+		String activationEvent;
+		String contentURL;
+		String buttonsContext;
+
+		public sendNotification(Context context)
+		{
+			super();
+			this.context = context;
+		}
+
+		@Override
+		protected Bitmap doInBackground(String... params)
+		{
+			InputStream in;
+
+			notificationID = Integer.parseInt(params[0]);
+			title = params[1];
+			details = params[2];
+			action = params[3];
+			activationEvent = params[4];
+			contentURL = params[5];
+			buttonsContext = params[6];
+
+			if (contentURL != null && contentURL.length() > 0)
+			{
+				try
+				{
+					in = new URL(contentURL).openStream();
+					Bitmap bmp = BitmapFactory.decodeStream(in);
+					return bmp;
+				}
+				catch (Exception e)
+				{
+					e.printStackTrace();
+				}
+			}
+
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(Bitmap result)
+		{
+			super.onPostExecute(result);
+
+//		Intent notificationIntent = new Intent(context, GameActivity.class);
+			Intent notificationIntent = new Intent(context, PsLocalNotificationsExtendedActionReceiver.class);
+//			notificationIntent.putExtra("local-notification-ID", notificationID);
+//			notificationIntent.putExtra("local-notification-action", action);
+//			notificationIntent.putExtra("local-notification-activationEvent", activationEvent);
+			notificationIntent.setAction("com.my.PsLocalNotificationsExtendedActionReceiver.Broadcast");
+
+			Log.d("onReceive", "NOID TASK: " + notificationID);
+		// launch if closed but resume if running
+			notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+//		notificationIntent.putExtra("localNotificationID" , notificationID);
+//		notificationIntent.putExtra("localNotificationAppLaunched" , true);
+//		notificationIntent.putExtra("localNotificationLaunchActivationEvent", activationEvent);
+
+			int notificationIconID = context.getResources().getIdentifier("ic_notification", "drawable", context.getPackageName());
+			if (notificationIconID == 0)
+			{
+				notificationIconID = context.getResources().getIdentifier("icon", "drawable", context.getPackageName());
+			}
+//		PendingIntent pendingNotificationIntent = PendingIntent.getActivity(context, notificationID, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+			PendingIntent pendingNotificationIntent = PendingIntent.getBroadcast(context, notificationID, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+			NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+				.setSmallIcon(notificationIconID)
+				.setContentIntent(pendingNotificationIntent)
+				.setWhen(System.currentTimeMillis())
+				.setTicker(details)		// note: will not show up on Lollipop up except for accessibility
+				.setContentTitle(title)
+				.setContentText(details)
+				.setVisibility(NotificationCompat.VISIBILITY_PUBLIC);
+
+			// Setting up image
+			if (result != null)
+			{
+				builder.setStyle(new NotificationCompat.BigPictureStyle().bigPicture(result));
+				Log.d("onReceive", "GOT URL: " + contentURL);
+			}
+
+			// Checking action buttons
+			if (buttonsContext.length() > 0)
+			{
+				Log.d("onReceive", "GOT_buttons: " + buttonsContext);
+				String[] buttons = buttonsContext.split("[|]");
+				for (int i = 0; i < buttons.length; i++)
+				{
+					String button = buttons[i];
+					String[] parts = button.split("=");
+					Log.d("onReceive", "GOT_buttons_parts: " + button);
+					if (parts.length > 1)
+					{
+						String actionTitle = parts[0];
+						String actionId = parts[1];
+						Log.d("onReceive", "GOT_buttons_title+id: " + actionTitle + " " + actionId);
+
+						if (actionTitle == null || actionId == null || actionTitle.length() == 0 || actionId.length() == 0)
+						{
+							break;
+						}
+
+						Intent notificationActionIntent = new Intent(context, PsLocalNotificationsExtendedActionReceiver.class);
+						notificationActionIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+						int actionNotificationID = notificationID;
+						notificationActionIntent.putExtra("local-notification-ID", actionNotificationID);
+						notificationActionIntent.putExtra("local-notification-action", action);
+						notificationActionIntent.putExtra("local-notification-activationEvent", activationEvent);
+						notificationActionIntent.putExtra("local-notification-activationAction", actionId);
+						notificationActionIntent.setAction("com.my.PsLocalNotificationsExtendedActionReceiver.Broadcast");
 
 //			PendingIntent actionPendingIntent = PendingIntent.getService(context, i, notificationActionIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-			PendingIntent actionPendingIntent = PendingIntent.getBroadcast(context, 0, notificationActionIntent, 0);
-			builder.addAction(android.R.drawable.star_on, actionTitle, pendingNotificationIntent);
-		}
+						PendingIntent actionPendingIntent = PendingIntent.getBroadcast(context, actionNotificationID, notificationActionIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+						builder.addAction(android.R.drawable.star_on, actionTitle, pendingNotificationIntent);
+					}
+				}
+			}
 
-		if (android.os.Build.VERSION.SDK_INT >= 26)
-		{
 			builder.setChannelId(GameActivity.PS_PUSH_NOTIFICATIONS_CHANNEL_ID);
-		}
-
-		if (android.os.Build.VERSION.SDK_INT >= 21)
-		{
 			builder.setColor(0xff0e1e43);
+
+			Notification notification = builder.build();
+
+			// Stick with the defaults
+			notification.flags |= Notification.FLAG_AUTO_CANCEL;
+			notification.defaults |= Notification.DEFAULT_SOUND | Notification.DEFAULT_VIBRATE;
+
+			NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+			// show the notification
+			notificationManager.notify(notificationID, notification);
 		}
-		Notification notification = builder.build();
-
-		// Stick with the defaults
-		notification.flags |= Notification.FLAG_AUTO_CANCEL;
-		notification.defaults |= Notification.DEFAULT_SOUND | Notification.DEFAULT_VIBRATE;
-
-		NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-
-		// show the notification
-		notificationManager.notify(notificationID, notification); 
-	}
+    };
 };
